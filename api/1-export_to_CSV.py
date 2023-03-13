f03aa3f03e795d3643d33c1f85996575a2931503
Ghassen69 <ghassenayari2000@gmail.com>
2023-03-13 16:30:12 +0000

    api-1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""gather data from an api"""
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    user_id = sys.argv[1]
+    response = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}")
+    username = response.json().get('username')
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    tasks = todos.json()
+
+    filename = f"{user_id}.csv"
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL)
+
+        for task in tasks:
+            if task.get('userId') == int(user_id):
+                task_completed = str(task.get('completed'))
+                task_title = task.get('title')
+                writer.writerow([user_id, username, task_completed, task_title])
