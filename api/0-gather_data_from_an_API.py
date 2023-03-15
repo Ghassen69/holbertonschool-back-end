667e2509ee67c500e7b79aba077fe36c33bf086e
Ghassen69 <ghassenayari2000@gmail.com>
2023-03-15 08:13:15 -0700

    p

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""Returns a list of completed tasks for a given employee ID"""
-
+""" Python Script """
 import requests
 from sys import argv
 
 if __name__ == "__main__":
-    employee = int(argv[1])
-    user = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(employee)
-    ).json()
-    TODO = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee)
-    ).json()
-    task_done = []
-    for task in TODO:
-        if task.get("completed") is True:
-            task_done.append(task.get("title"))
-    print(
-        "Employee {} is done with tasks({}/{}):".format(
-            user.get("name"), len(task_done), len(TODO)
-        )
-    )
-    for done in task_done:
-        print("\t {}".format(done))
+    u_link = "https://jsonplaceholder.typicode.com/users"
+    uid = argv[1]
+
+    u_info = requests.get("{}/{}".format(u_link, uid)).json()
+    e_name = u_info['name']
+
+    todo_info = requests.get("{}/{}/todos".format(u_link, uid)).json()
+    done = list()
+    for todo in todo_info:
+        if todo['completed'] is True:
+            done.append(todo['title'])
+    total = len(todo_info)
+    total_done = len(done)
+
+    string = "Employee {} is done with tasks({}/{}):".format(
+        e_name, total_done, total)
+    print(string)
+    for task in done:
+        print("\t ", task)
--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -19,4 +19,3 @@ if __name__ == "__main__":
     json_file_name = sys.argv[1] + ".json"
     with open(json_file_name, "w") as json_file:
         json.dump(tasks_dict, json_file)
-
