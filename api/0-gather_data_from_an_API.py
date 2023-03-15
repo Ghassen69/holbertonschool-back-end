a0f8303dbbaa5250b3208b69fcc799232031d22c
Ghassen69 <ghassenayari2000@gmail.com>
2023-03-15 07:01:00 -0700

    llwn

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,11 +6,7 @@ import sys
 
 
 if __name__ == '__main__':
-    employee_id = sys.argv[1]
-    employee = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                            .format(employee_id))
-
-    employee_name = employee.json().get('name')
+ame = employee.json().get('name')
 
     tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
     completed_count = 0
--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Gather data using an API."""
 
 import csv
